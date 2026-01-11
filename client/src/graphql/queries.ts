fcf1b94a98e77d1ed4bc2b834551a516e7f05867
TraianBurlacu <jomirajo@gmail.com>
2026-01-11 21:09:31 +0200

    register/login page added

--- client/src/graphql/queries.ts
+++ client/src/graphql/queries.ts
@@ -32,3 +32,28 @@ export const ADD_REVIEW = gql`
     }
   }
 `;
+
+export const REGISTER_USER = gql`
+  mutation RegisterUser($name: String!, $email: String!, $password: String!, $role: Role!) {
+    registerUser(name: $name, email: $email, password: $password, role: $role) {
+      id
+      name
+      email
+      role
+    }
+  }
+`;
+
+export const LOGIN_USER = gql`
+  mutation LoginUser($email: String!, $password: String!) {
+    loginUser(email: $email, password: $password) {
+      token
+      user {
+        id
+        name
+        email
+        role
+      }
+    }
+  }
+`;
